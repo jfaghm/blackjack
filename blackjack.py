5bfc3548e22452e7ae9e22409edc892c7f87e93a
James Faghmous <james@Jamess-MacBook-Air.local>
2014-07-08 23:50:24 -0500

    added bust probability calculations

--- blackjack.py
+++ blackjack.py
@@ -8,9 +8,10 @@ Copyright (c) 2014 __MyCompanyName__. All rights reserved.
 """
 #to do:
 ## 1- Show the dealer's card
-## 2- Allow for double?
-## 3- fix the '52' number in the probablity function
-## 4- fix the probability of busting
+## 2- fix gameplay presentation/prompts
+## 3- Allow for double?
+## 4- fix the '52' number in the probablity function
+
 
 import sys
 import os
@@ -52,7 +53,7 @@ class Hand():
 		new_card = deck.draw()
 		self.cards.append(new_card)
 		print(self.owner + ' drew a ' + new_card)
-		self.total = self.total + deck.values[new_card]
+		self.total = self.total + deck.values_lookup[new_card]
 		#automatically take care of the "soft" "hard" business
 		if "A" in self.cards and self.total + 10 <=21:
 			self.total = self.total + 10
@@ -65,13 +66,6 @@ class Hand():
 		del self.cards[:]
 		self.total=0
 
-	def bust_probability(self,up_card):
-		global deck
-		margin = 21 - self.total
-		over_margin = sum(deck.cards >= margin for c in deck.cards)
-		return over_margin/52
-
-
 class Game():
 	def __init__(self, player, dealer, stats):
 		self.player = player
@@ -79,7 +73,7 @@ class Game():
 		self.stats = stats
 		
 	def hit_or_stand(self):
-		choice = raw_input('Continue or stop? You have a ' + str(self.player.hand.bust_probability(self.dealer.hand.cards[0])) + ' probability of busting')
+		choice = raw_input('Continue or stop? You have a ' + str(self.get_bust_probability(self.player.hand,self.dealer.hand)) + ' percent probability of busting')
 		if choice == "q":
 			return 0
 		else:
@@ -108,11 +102,13 @@ class Game():
 		self.dealer.hand.clear_hand()
 		initial_bet = 0
 		##player turn##
-		while initial_bet < 1:
+		while initial_bet < 1 or initial_bet > self.player.credits:
 			try:
 				initial_bet = int(raw_input('How much would you like to bet? You have ' + str(self.player.credits) + ' credits. '))
 				if initial_bet < 1:
 					print('Please bet at least 1 credit')
+				if initial_bet > self.player.credits:
+					print('You do not have sufficent credits to make this wager. You have ' + str(self.player.credits) + ' credits left.')
 			except ValueError:
 				print('That was an invalide number. Please enter a value >= 1')
 				
@@ -124,7 +120,13 @@ class Game():
 			self.player.hand.draw_card()
 			self.dealer.hand.draw_card()
 		self.player.hand.show_hand()
-		hit = self.hit_or_stand()
+		if self.player.hand.total <  21:
+			hit = self.hit_or_stand()
+		if self.player.hand.total == 21:
+			print('Player Blackjack!')
+			self.increment_stats('player', 'blackjack')
+			self.player.change_credits(initial_bet*2.5) #3:2 retunrs for blackjack
+			
 		while self.player.hand.total < 21 and hit:
 			self.player.hand.draw_card()
 			self.player.hand.show_hand()
@@ -132,11 +134,11 @@ class Game():
 				print('Player bust!')
 				self.increment_stats('player', 'lose')
 				break
-			if self.player.hand.total == 21 and len(self.player.hand.cards) == 2:
-				print('Player Blackjack!')
-				self.increment_stats('player', 'blackjack')
-				self.player.change_credits(initial_bet*2.5) #3:2 retunrs for blackjack
-				break
+			# if self.player.hand.total == 21 and len(self.player.hand.cards) == 2:
+			# 			print('Player Blackjack!')
+			# 			self.increment_stats('player', 'blackjack')
+			# 			self.player.change_credits(initial_bet*2.5) #3:2 retunrs for blackjack
+			# 			break
 			#should I check for 21 with multiple cards?
 			#if self.player.hand.total == 21 and len(self.player.hand.cards) > 2:
 				
@@ -171,17 +173,26 @@ class Game():
 					break
 		print('Your current credit is: ' + str(self.player.credits))
 			 	
-		
+	def get_bust_probability(self,player_hand,dealer_hand):
+		#we need a new deck to get the proper counts
+		global deck
+		margin = 21 - player_hand.total
+		deck.card_values.append(deck.values_lookup[dealer_hand.cards[1]]) #we need to put back the dealer's hidden card since we cannot account for it in the probabilities
+		over_margin = len([c for c in deck.card_values if c > margin]) 
+		deck.card_values.remove(deck.values_lookup[dealer_hand.cards[1]]) #remove the dealer's hidden card that we had inserted to compute accurate probabilities
+		return round((over_margin/52.0)*100.0)
 	
 class Deck():
 	def __init__(self):
-		self.values = {'A':1,'2':2,'3':3,'4':4, '5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'K':10,'Q':10}
-		self.cards = list(self.values.keys())*4
+		self.values_lookup = {'A':1,'2':2,'3':3,'4':4, '5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'K':10,'Q':10}
+		self.cards = list(self.values_lookup.keys())*4
+		self.card_values = list(self.values_lookup.values())*4
 		
 	def shuffle(self):
 		random.shuffle(self.cards)
 				
 	def draw(self):
+		self.card_values.remove(self.values_lookup[self.cards[0]]) #update the values list
 		return self.cards.pop(0)
 	
 	def cards_left(self):
